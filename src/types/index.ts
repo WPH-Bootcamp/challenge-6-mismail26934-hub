d8641c3f38a8d674b5d8485a18596573a8d24cc8
40-Muhammad Ismail <m.ismail.26934@gmail.com>
2026-05-16 14:44:42 +0800

    Define type book

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,10 @@
 // Petunjuk: gunakan type alias atau interface untuk mendefinisikan struktur data ini
 // Pastikan tipe data untuk setiap properti sudah sesuai dengan kebutuhan
 
+export type Book = {
+  title: string;
+  author: string;
+  publicationYear: number;
+};
+
+export const books: Book[] = [];
